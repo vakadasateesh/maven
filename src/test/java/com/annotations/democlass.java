a45bd4f5a90d751d56b0ddd5b5844dc8cf1c393b
vakadasateesh <vakadasateesh022196@gmail.com>
2023-01-03 14:28:14 +0530

    i added democlass

---
+++ src/test/java/com/annotations/democlass.java
@@ -0,0 +1,24 @@
+package com.annotations;
+
+import org.testng.annotations.Test;
+
+public class democlass {
+	
+	@Test
+	public void myname() {
+	String myname;
+		myname="sateesh";
+		System.out.println(myname);
+	
+
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
